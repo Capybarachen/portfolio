d8b28710851e92c01383a601fead2ca49220b63d
Brain <poc008@ucsd.edu>
2026-04-17 18:10:21 -0700

    Implement dynamic navigation bar
    Add a dynamic navigation bar with links to various pages.

--- global.js
+++ global.js
@@ -3,3 +3,43 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "cv.html", title: "CV" },
+  { url: "https://github.com/Capybarachen", title: "GitHub" }
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+
+  if (!url.startsWith("http")) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = p.title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
+
+console.log("Navbar created successfully");
