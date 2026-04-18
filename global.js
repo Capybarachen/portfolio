93329e4f3ad7bf298b676faa1a2f0b9f22f7aad6
Brain <poc008@ucsd.edu>
2026-04-17 18:19:38 -0700

    Implement theme selection in global.js
    Add theme selection dropdown to the document body

--- global.js
+++ global.js
@@ -43,3 +43,17 @@ for (let p of pages) {
 }
 
 console.log("Navbar created successfully");
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+<label class="color-scheme">
+Theme:
+<select>
+  <option value="light dark">Auto</option>
+  <option value="light">Light</option>
+  <option value="dark">Dark</option>
+</select>
+</label>
+`
+);
