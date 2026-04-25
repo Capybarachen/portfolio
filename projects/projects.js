73cc8b83d64da37d49498f75fab71be7de1673ae
Brain <poc008@ucsd.edu>
2026-04-24 23:04:16 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,6 @@
 const projects = await fetchJSON('./lib/projects.json');
 
-projects.sort((a, b) => b.year - a.year);
+projects.sort((a, b) => Number(b.year) - Number(a.year));
 
 const container = document.querySelector('.projects');
 
