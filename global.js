bd240d18781f46067aaede7894d111516d7ff522
Brain <poc008@ucsd.edu>
2026-04-24 22:14:49 -0700

    Add fetchGithubData function to retrieve user data

--- global.js
+++ global.js
@@ -125,3 +125,7 @@ export function renderProjects(projects, container, headingLevel = "h2") {
     container.appendChild(article);
   }
 }
+
+export async function fetchGithubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
