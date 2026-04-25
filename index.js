080e89288ee46575998dfdb109bd979f11196dd8
Brain <poc008@ucsd.edu>
2026-04-24 20:53:35 -0700

    Add project rendering functionality in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import { fetchJSON, renderProjects } from './global.js';
+
+const projects = await fetchJSON('./lib/projects.json');
+
+const latest = projects.slice(0, 3);
+
+const container = document.querySelector('.projects');
+
+if (container) {
+  renderProjects(latest, container, 'h2');
+}
