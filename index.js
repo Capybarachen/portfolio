40cacbb3b65444c13abd074b905fbc8fae2a04cf
Brain <poc008@ucsd.edu>
2026-04-24 23:05:20 -0700

    Sort projects by year and check githubData

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 
+projects.sort((a, b) => Number(b.year) - Number(a.year));
+
 const latest = projects.slice(0, 3);
 
 const container = document.querySelector('.projects');
@@ -10,12 +12,11 @@ if (container) {
   renderProjects(latest, container, 'h2');
 }
 
-
 const githubData = await fetchGithubData('Capybarachen');
 
 const statsContainer = document.querySelector('#profile-stats');
 
-if (statsContainer) {
+if (statsContainer && githubData) {
   statsContainer.innerHTML = `
     <p>Repos: ${githubData.public_repos}</p>
     <p>Followers: ${githubData.followers}</p>
