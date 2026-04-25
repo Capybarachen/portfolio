6d1dc29e87c9b27eb59f35253e992c62d135b7ad
Brain <poc008@ucsd.edu>
2026-04-24 23:12:59 -0700

    Add null check before rendering projects
    Ensure projects are rendered only if container and projects exist.

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,11 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+const projects = await fetchJSON('/portfolio/lib/projects.json');
+
 projects.sort((a, b) => Number(b.year) - Number(a.year));
 
 const container = document.querySelector('.projects');
 
-renderProjects(projects, container, 'h2');
+if (container && projects) {
+  renderProjects(projects, container, 'h2');
+}
