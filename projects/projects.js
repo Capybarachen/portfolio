121238abdb1553e5771e6f8c043659b1a647280b
Brain <poc008@ucsd.edu>
2026-05-01 15:46:55 -0700

    Integrate D3.js and add pie plot visualization
    Added D3.js import and created a red pie plot.

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('/portfolio/lib/projects.json');
@@ -9,3 +11,20 @@ const container = document.querySelector('.projects');
 if (container && projects) {
   renderProjects(projects, container, 'h2');
 }
+
+// red plot test
+
+const svg = d3.select('#projects-pie-plot');
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI
+});
+
+svg.append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
